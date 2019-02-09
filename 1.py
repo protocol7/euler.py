b9b48a27b717684f2c75c048c96e6e809acf46dd
Niklas Gustavsson <niklas@protocol7.com>
2019-02-09 22:05:45 +0100

    Clean up 1

--- 1.py
+++ 1.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 
-def problem1():
-    return sum(filter(lambda i: i % 3 == 0 or i % 5 == 0, range(1, 1000)))
-
-assert 233168 == problem1()
+assert 233168 == sum(filter(lambda i: i % 3 == 0 or i % 5 == 0, range(1, 1000)))

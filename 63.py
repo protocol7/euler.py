6e7da0548ab2378f66346f813ba5c57781745007
Niklas Gustavsson <niklas@protocol7.com>
2019-03-10 10:22:23 +0100

    63

---
+++ 63.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from itertools import count
+
+def nlen(n):
+    return len(str(n))
+
+found = set()
+for a in range(1, 100):
+    for b in range(1, 100):
+        n = a**b
+        if nlen(n) == b:
+            found.add(n)
+
+print(len(found))
